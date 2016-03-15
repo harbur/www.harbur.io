61ac180d7be1aa18dce155a48bc644356923abf7
spiddy <d.kapanidis@gmail.com>
2016-03-15 20:25:48 +0100

    Added install-hugo script

---
+++ scripts/install-hugo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+HUGO_VERSION=0.14
+wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz
+tar xvzf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
+cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo
