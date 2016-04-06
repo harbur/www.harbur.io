9418e802fdb26dfe72b92805fa4759209c4b3fe4
spiddy <d.kapanidis@gmail.com>
2016-04-06 18:37:19 +0200

    Upgrade hugo to 0.15

--- scripts/install-hugo.sh
+++ scripts/install-hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-HUGO_VERSION=0.14
+HUGO_VERSION=0.15
 wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz
 tar xvzf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
 cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo
